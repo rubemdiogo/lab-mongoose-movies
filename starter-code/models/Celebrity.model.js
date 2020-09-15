bd2fef179bbc186b19b62d29e8371d8a1eb8b98f
Rubem Diogo <rubemdiogo@gmail.com>
2020-09-15 17:45:12 -0400

    01, 02, ok

---
+++ starter-code/models/Celebrity.model.js
@@ -0,0 +1,8 @@
+const { Schema, model } = require("mongoose");
+const CelebritySchema = new Schema({
+  name: String,
+  occupation: String,
+  catchPhrase: String,
+});
+
+module.exports = model("Celebrity", CelebritySchema);
